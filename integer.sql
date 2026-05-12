f6698168bfbfab457364bd841a66cb3001d215a9
venessie <venplayz527@gmail.com>
2026-05-12 18:35:04 +0800

    tuesday may 12th 2026

---
+++ integer.sql
@@ -0,0 +1,15 @@
+CREATE TABLE panda (
+    SNO TEXT PRIMARY KEY,
+    SNAME TEXT,
+    STATUS INTEGER,
+    CITY TEXT
+);
+
+INSERT INTO panda (SNO, SNAME STATUS, CITY) VALUES
+('S1', 'Smith', 20, 'London'),
+('S2', 'Jones', 10, 'Paris'),
+('S3', 'Blake', 30, 'Paris'),
+('S4', 'Clarke', 20, 'London'),
+('S5', 'Adams', 30, 'Athens');
+
+SELECT * FROM panda ;
