6a6b5d1e152b7969079a76f8fb1934482c211b7a
venessie <venplayz527@gmail.com>
2026-05-14 18:25:07 +0800

    thrusday may 14th 2026

--- capstone.sql
+++ capstone.sql
@@ -16,3 +16,34 @@ SELECT * FROM STUDY_GROUP;
 SELECT * FROM STUDY_GROUP WHERE AGE = 12;
 
 SELECT * FROM STUDY_GROUP WHERE BEST_SUBJECT = 'Math';
+
+
+
+
+CREATE TABLE IF NOT EXISTS PRODUCT(
+    PRO_ID TEXT PRIMARY KEY,
+    PRO_NAME TEXT,
+    PRO_PRICE TEXT,
+    PRO_COM TEXT
+);
+
+INSERT INTO PRODUCT(PRO_ID,PRO_NAME,PRO_PRICE,_PRO_COM)
+VALUES
+    ("101", "MOTHER BOARD","3200","15"),
+    ("102", "KEY BOARD", "450", "16"),
+    ("103", "ZIP DRIVE", "250", "14"),
+    ("104", "SPEAKER", "550", "16"), 
+    ("105", "MONITOR", "5000", "11"), 
+    ("106", "DVD DRIVE", "900", "12"), 
+    ("107", "CD DRIVE", "800", "12"), 
+    ("108", "PRINTER", "2600", "13"), 
+    ("109", "REFILL CARTRIDGE", "350", "13"),
+    ("110", "MOUSE", "250","12");
+SELECT pro_name, pro_price
+    FROM PRODUCT
+    WHERE pro_price =
+    (SELECT MIN(proprice) FROM PRODUCT);
+SELECT pro_name, pro_price
+    FROM PRODUCT
+    WHERE pro_price =
+    (SELECT MAX(pro_price) FROM PRODUCT);
